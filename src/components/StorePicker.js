f6212851901be416e1cdb43ec50f535d6a4b4be4
Irma <irblancofdz@gmail.com>
2018-07-29 20:50:34 +0200

    Scaffolding the app

---
+++ src/components/StorePicker.js
@@ -0,0 +1,19 @@
+import React, { Component, Fragment } from 'react';
+
+
+class StorePicker extends Component {
+  render(){
+    return (
+      <Fragment>
+        <form className="store-selector">
+          <h2>Please enter a Store</h2>
+          <input type="text" required placeholder="Store Name" />
+          <button type="submit">Visit Store</button>
+        </form>
+      </Fragment>
+    )
+  }
+}
+
+export default StorePicker;
+
